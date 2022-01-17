a9c07ae2f281c174a7c895c354b57d30d7361b65
Md Mahbub Rahman <54384173+mahbub099@users.noreply.github.com>
2022-01-17 23:51:49 +0600

    Create coronawar.sql

---
+++ sql file/coronawar.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 17, 2022 at 06:50 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coronawar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `ID` int(20) NOT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `PASS` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`ID`, `NAME`, `EMAIL`, `PASS`) VALUES
+(1, 'Admin', 'admin@gmail.com', 81);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `TYPE` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `PASS` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`ID`, `NAME`, `TYPE`, `EMAIL`, `PASS`) VALUES
+(4, 'Mahbub rahman    ', 'Coronavirus Specialist', 'mrm@gmail.com', 81),
+(5, 'Rakib Mahmud', 'Coronavirus expert', 'rakib@gmail.com', 81),
+(8, 'Rahim', 'Corona Expert', 'rahim@gmail.com', 81),
+(16, 'Farhana Khan', 'medicine', 'farhanasoha22@gmail.com', 9),
+(20, 'Naeem Rafsan', 'medicine', 'naeem5d97@gmail.com', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `PASS` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`ID`, `NAME`, `EMAIL`, `PASS`) VALUES
+(1, 'Naeem Hossain', 'naeem@gmail.com', 81),
+(12, 'Al Mamun', 'm@gmail.com', 81),
+(13, 'Likhon', 'l@gmail.com', 81);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visit`
+--
+
+CREATE TABLE `visit` (
+  `ID` int(11) NOT NULL,
+  `D_NAME` varchar(255) NOT NULL,
+  `P_NAME` varchar(255) NOT NULL,
+  `S_TYPE` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `visit`
+--
+
+INSERT INTO `visit` (`ID`, `D_NAME`, `P_NAME`, `S_TYPE`) VALUES
+(28, 'Mahbub rahman    ', 'Rakib Hassan', 'Vaccine Push'),
+(43, 'Mahbub rahman    ', 'Naeem', 'Antibody Test'),
+(44, 'Mahbub rahman    ', 'Naeem Hossain', 'Corona Test'),
+(46, 'Mahbub rahman    ', 'Likhon', 'Antibody Test'),
+(47, 'Mahbub rahman    ', 'Likhon', 'Corona Test'),
+(48, 'Mahbub rahman    ', 'Rakib Hassan', 'Vaccine Push'),
+(53, 'Mahbub rahman', 'Naeem Hossain', 'Corona Test');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `visit`
+--
+ALTER TABLE `visit`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `visit`
+--
+ALTER TABLE `visit`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
