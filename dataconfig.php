53ab0cb442a4cdb0d2c4ee536a82e3724f1341ec
nerdigenius <Ashiq.racer@gmail.com>
2022-01-17 22:38:28 +0600

    patient part done

---
+++ dataconfig.php
@@ -0,0 +1,17 @@
+<?php
+
+$host="localhost";
+$user="root";
+$pass="";
+$datab="coronawar";
+
+$conn=new mysqli($host,$user,$pass,$datab);
+
+if($conn -> connect_error ){
+	die($conn -> error);
+}
+else{
+	// echo "Database Connected";
+}
+
+?>
