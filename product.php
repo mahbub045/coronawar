fa126a98db107c0eb7418bd7bfef9ae60e7dd7ce
Zayaan Nashrid <snashrid161127@bscse.uiu.ac.bd>
2022-01-17 20:52:01 +0600

    Added Link from product.php to checkout page and created checkout page.

--- product.php
+++ product.php
@@ -8,9 +8,10 @@
 	$result_doc=$conn->query($selectsql);
 	
 	echo "<table>";
-	echo "<tr> <th> Product ID </th> <th> Product </th> <th> Price </th> <th> Quantity Available </th> </tr>";
+	echo "<tr> <th> Product ID </th> <th> Product </th> <th> Price </th> <th> Quantity Available </th> <th>  </th></tr>";
 	while($row = $result_doc->fetch_assoc()){
-		echo '<tr><td>' . $row["Product_ID"] . '</td><td>' . $row["Product_Name"] . '</td><td>' . $row["Product_Cost"] . '</td><td>' . $row["Quantity_Available"] . '</td></tr>';
+		echo '<tr><td>' . $row["Product_ID"] . '</td><td>' . $row["Product_Name"] . '</td><td>' . $row["Product_Cost"] . '</td><td>' . $row["Quantity_Available"] . '</td><td>';
+		echo  "<a href = 'product_checkout.php'>Buy</a> </td></tr>";
 	}
 
 	echo "</table>";
