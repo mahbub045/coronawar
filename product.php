41941d9b3fabc9d2a2ea021c35ba02b68ff5e6b4
Zayaan Nashrid <snashrid161127@bscse.uiu.ac.bd>
2022-01-20 21:17:43 +0600

    Code Cleanup + removed excess printing

--- product.php
+++ product.php
@@ -11,9 +11,9 @@
 	echo "<tr> <th> Product ID </th> <th> Product </th> <th> Price </th> <th>  </th></tr>";
 	while($row = $result_doc->fetch_assoc()){
 		$id = $row["Product_ID"]; $name = $row["Product_Name"];
-		echo $name;
+		#echo $name;
 		echo '<tr><td>' . $id . '</td><td>' . $row["Product_Name"] . '</td><td>' . $row["Product_Cost"] . '</td><td>';
-		echo  '<form method="post" action="product_checkout.php"> <input type="hidden" name = "P_ID" id="ID" value = '.$id.' > > <br> <input type= "submit" value = "Buy"> </form> </td></tr>';
+		echo  '<form method="post" action="product_checkout.php"> <input type="hidden" name = "P_ID" id="ID" value = '.$id.' ><input type= "submit" value = "Buy"> </form> </td></tr>';
 		#echo $id."<br>";
 	}
 
