9e28ff8363fe027354c55c85e29a54b4ef9ec4e4
Zayaan Nashrid <snashrid161127@bscse.uiu.ac.bd>
2022-01-20 21:15:44 +0600

    Finished Product Transfer Code

--- product.php
+++ product.php
@@ -2,16 +2,19 @@
 	
 	include("dataconfig.php");
 	
-	$selectsql="SELECT * FROM products WHERE Quantity_Available > 0";
+	$selectsql="SELECT * FROM products";
 
 	$conn->query($selectsql);
 	$result_doc=$conn->query($selectsql);
 	
 	echo "<table>";
-	echo "<tr> <th> Product ID </th> <th> Product </th> <th> Price </th> <th> Quantity Available </th> <th>  </th></tr>";
+	echo "<tr> <th> Product ID </th> <th> Product </th> <th> Price </th> <th>  </th></tr>";
 	while($row = $result_doc->fetch_assoc()){
-		echo '<tr><td>' . $row["Product_ID"] . '</td><td>' . $row["Product_Name"] . '</td><td>' . $row["Product_Cost"] . '</td><td>' . $row["Quantity_Available"] . '</td><td>';
-		echo  "<a href = 'product_checkout.php'>Buy</a> </td></tr>";
+		$id = $row["Product_ID"]; $name = $row["Product_Name"];
+		echo $name;
+		echo '<tr><td>' . $id . '</td><td>' . $row["Product_Name"] . '</td><td>' . $row["Product_Cost"] . '</td><td>';
+		echo  '<form method="post" action="product_checkout.php"> <input type="hidden" name = "P_ID" id="ID" value = '.$id.' > > <br> <input type= "submit" value = "Buy"> </form> </td></tr>';
+		#echo $id."<br>";
 	}
 
 	echo "</table>";
--- product_checkout.php
+++ product_checkout.php
@@ -1,5 +1,32 @@
-<html>
-	<body>
+<?php
+	echo "<html> <body>";
+	$P_ID = $_POST['P_ID'];
+	
+	#$P_NAME = $_POST['P_Name'];
+	#$P_COST = $_POST['P_COST'];
+	
+	
+	include("dataconfig.php");
+	
+	$selectsql= "SELECT * FROM products WHERE Product_ID = '$P_ID'";
+
+	$conn->query($selectsql);
+	$result_doc=$conn->query($selectsql);
+	$row = $result_doc->fetch_assoc();
+	
+	$P_ID = $row["Product_ID"];
+	$P_Name = $row["Product_Name"];
+	$P_Cost = $row["Product_Cost"];
+	
+	$conn->close();	
+	
+	
+	echo '<br>Your Order of '. $P_Name .' is ready. Total Price is:  Tk' . $P_Cost . ' Please fill out the details below </br>';
+	
+	
+?>
+	
+	<br>
 		<form action="product_checkout_confirmation.php" method="post">
 			Name: <input type="text" name="name"><br>
 			E-mail: <input type="text" name="email"><br>
